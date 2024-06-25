713c70a45f48708a4b2ddcf13523fc25e317c863
jayylmao <jaymichael_carlos@icloud.com>
2024-06-26 07:38:10 +0800

    fix: add check for invalid date in displayCalendar

--- Test.java
+++ Test.java
@@ -1,11 +1,8 @@
 public class Test {
 	public static void main(String[] args) {
-		Reservation reservation = new Reservation(new Date(6), new Date(8), 1299, new Guest("Jay", "Carlos"));
+		Driver driver = new Driver();
+		Room room = new Room("004");
 
-		System.out.println(reservation.getCheckIn().getFormattedDate());
-		System.out.println(reservation.getCheckOut().getFormattedDate());
-		System.out.println(reservation.getReservedPrice());
-		System.out.println(reservation.getGuests().get(0).getName());
-		System.out.println(reservation.calculateTotalPrice(new Date(6), new Date(4)));
+		driver.displayCalendar(room);
 	}
 }
