796ab2d461a289a00ba48e53d409aa565e19f7f2
Latzero <tabehiroyuki@gmail.com>
2024-07-30 20:23:16 +0800

    added documentation for the room subclasses

--- StandardRoom.java
+++ StandardRoom.java
@@ -2,6 +2,11 @@
  * The Standard Room class defines a standard room in a hotel, with its corresponding details and base price.
  */
 public class StandardRoom extends Room {
+	/**
+	 * Regular Constructor that creates a Standard Room instance.
+	 * @param roomName The name of the Room. Should be unique compared to other Room instances.
+	 * @param roomPrice Base price for a room.
+	 */
 	public StandardRoom(String roomName, double roomPrice) {
 		super(roomName, roomPrice);
 	}
